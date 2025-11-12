4e6dae0f5200b0373b624fc1aab6f6d590f49162
Rafael Silva <rafaelsilvar221@gmail.com>
2025-11-12 18:41:25 -0300

    first commit

---
+++ types/generated/components.d.ts
@@ -0,0 +1,68 @@
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface BlocksFooter extends Struct.ComponentSchema {
+  collectionName: 'components_blocks_footers';
+  info: {
+    displayName: 'Footer';
+    icon: 'grid';
+  };
+  attributes: {
+    copyrightText: Schema.Attribute.String;
+    description: Schema.Attribute.Text;
+    linksColumn1: Schema.Attribute.Component<'blocks.hero-section', true>;
+    linksColumn2: Schema.Attribute.Component<'blocks.hero-section', true>;
+    linksColumn3: Schema.Attribute.Component<'blocks.logo-link', true>;
+  };
+}
+
+export interface BlocksHeader extends Struct.ComponentSchema {
+  collectionName: 'components_blocks_headers';
+  info: {
+    displayName: 'Header';
+    icon: 'apps';
+  };
+  attributes: {
+    logo: Schema.Attribute.Media<'images'>;
+    navigation_links: Schema.Attribute.Component<'blocks.hero-section', true>;
+  };
+}
+
+export interface BlocksHeroSection extends Struct.ComponentSchema {
+  collectionName: 'components_blocks_hero_sections';
+  info: {
+    displayName: 'LinkMenu';
+    icon: 'bulletList';
+  };
+  attributes: {
+    isButtonLink: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
+    isOpenNewTab: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
+    label: Schema.Attribute.Text;
+    type: Schema.Attribute.Enumeration<['primary', 'secondary']>;
+    url: Schema.Attribute.String;
+  };
+}
+
+export interface BlocksLogoLink extends Struct.ComponentSchema {
+  collectionName: 'components_blocks_logo_links';
+  info: {
+    displayName: 'Logo Link';
+    icon: 'emotionHappy';
+  };
+  attributes: {
+    href: Schema.Attribute.String;
+    image: Schema.Attribute.Media<'images'>;
+    isExternal: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
+    label: Schema.Attribute.String;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'blocks.footer': BlocksFooter;
+      'blocks.header': BlocksHeader;
+      'blocks.hero-section': BlocksHeroSection;
+      'blocks.logo-link': BlocksLogoLink;
+    }
+  }
+}
